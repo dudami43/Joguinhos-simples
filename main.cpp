dc4f3f6e95a18e907dc549482ecc6616d89045c5
maria.miranda <maria.miranda@nobugs.com.br>
2018-09-07 21:00:02 -0300

    Batalha naval

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ using namespace std;
 GLint tipo;
 int xclick, yclick;
 int quadrados [10][8], erros = 0, acertos = 0, rodadas = 0, cliques = 10;
-int bombas = 0, embarcacoes = 0, tabuleiro[40][40];
-bool primeiro_desenho = true, clicado = false;
+int bombas = 0, embarcacoes = 0, tabuleiro[40][40], vidas = 3;
+bool primeiro_desenho = true, clicado = false, ganhou = true;
 char texto[50];
 
 void display()
@@ -289,44 +289,165 @@ void MemoriaDificil(int x, int y)
 
 void TelaBatalhaNaval()
 {
-    glColor3f(0.91f, 0.91f, 0.70f); 
-    for(int i=0;i<40; i++)
+    if(primeiro_desenho)
+    {
+        primeiro_desenho = false;
+        glColor3f(0.91f, 0.91f, 0.70f); 
+        for(int i=0;i<40; i++)
+        {
+            for(int j=0; j<40; j++)
+            {
+                DesenhaQuadrado(5+(11*j),5+(11*j),2+(11*(j+1)),2+(11*(j+1)),11*(i+1),2+(11*i),2+(11*i),11*(i+1)); 
+                tabuleiro[i][j] = 0;
+            }
+        }
+        
+        int col_rand,lin_rand, embarcacao_rand;
+        
+        while(embarcacoes<10)
+        {
+            col_rand = (rand()%40);
+            lin_rand = (rand()%40);
+            embarcacao_rand = rand()%4;
+            switch(embarcacao_rand)
+            {
+                case 0: //Destroyer
+                    if(lin_rand<40 && col_rand<35 && tabuleiro[lin_rand][col_rand] == 0
+                    && tabuleiro[lin_rand][col_rand+1] == 0 && tabuleiro[lin_rand-1][col_rand+1] == 0
+                    && tabuleiro[lin_rand-1][col_rand+1] == 0 && tabuleiro[lin_rand-1][col_rand+2] == 0
+                    && tabuleiro[lin_rand-1][col_rand+3] == 0)
+                    {
+                        tabuleiro[lin_rand][col_rand] = tabuleiro[lin_rand][col_rand+1] =tabuleiro[lin_rand-1][col_rand+1] = 1;
+                        tabuleiro[lin_rand-1][col_rand+1] = tabuleiro[lin_rand-1][col_rand+2]=tabuleiro[lin_rand-1][col_rand+3] = 1;
+                        embarcacoes++;
+                        cout << "Embarcação: " << lin_rand << " " << col_rand << endl;
+                    }
+                case 1: //Porta-aviões
+                    if(lin_rand>0 && col_rand<34 && tabuleiro[lin_rand][col_rand] == 0
+                    && tabuleiro[lin_rand][col_rand+1] == 0 && tabuleiro[lin_rand][col_rand+2]== 0
+                    && tabuleiro[lin_rand][col_rand+3] == 0 && tabuleiro[lin_rand-1][col_rand+2] == 0 && tabuleiro[lin_rand-1][col_rand+3] == 0
+                    && tabuleiro[lin_rand-1][col_rand+4] == 0 && tabuleiro[lin_rand-1][col_rand+5] == 0)
+                    {
+                        tabuleiro[lin_rand][col_rand] = tabuleiro[lin_rand][col_rand+1] = tabuleiro[lin_rand][col_rand+2] = 1;
+                        tabuleiro[lin_rand][col_rand+3] = tabuleiro[lin_rand-1][col_rand+2] = tabuleiro[lin_rand-1][col_rand+3] = 1;
+                        tabuleiro[lin_rand-1][col_rand+4] = tabuleiro[lin_rand-1][col_rand+5] = 1;
+                        embarcacoes++;
+                        cout << "Embarcação: " << lin_rand << " " << col_rand << endl;
+                    }
+                case 2: //Lança de ataque
+                    if(lin_rand>0 && col_rand<37 &&  tabuleiro[lin_rand][col_rand] == 0
+                    && tabuleiro[lin_rand][col_rand+1] == 0 && tabuleiro[lin_rand-1][col_rand+2] == 0)
+                    {   
+                        tabuleiro[lin_rand][col_rand] = tabuleiro[lin_rand][col_rand+1] = tabuleiro[lin_rand-1][col_rand+2]= 1;
+                        embarcacoes++;
+                        cout << "Embarcação: " << lin_rand << " " << col_rand << endl;
+                    }
+                case 3: //Submarino
+                    if(lin_rand<39 && col_rand<35 && tabuleiro[lin_rand][col_rand] == 0 && tabuleiro[lin_rand][col_rand+1] == 0
+                    && tabuleiro[lin_rand][col_rand+2] == 0 && tabuleiro[lin_rand][col_rand+3] == 0
+                    && tabuleiro[lin_rand][col_rand+4] == 0 && tabuleiro[lin_rand+1][col_rand+2] == 0)
+                    {
+                        tabuleiro[lin_rand][col_rand] = tabuleiro[lin_rand][col_rand+1] = tabuleiro[lin_rand][col_rand+2] = 1;
+                        tabuleiro[lin_rand][col_rand+3] = tabuleiro[lin_rand][col_rand+4] = tabuleiro[lin_rand+1][col_rand+2] = 1;
+                        embarcacoes++;
+                        cout << "Embarcação: " << lin_rand << " " << col_rand << endl;
+                    }
+                case 4: //Corveta
+                    if(lin_rand>0 && col_rand<38 && tabuleiro[lin_rand][col_rand] == 0 && tabuleiro[lin_rand][col_rand+1] == 0
+                    && tabuleiro[lin_rand-1][col_rand+1] == 0)
+                    {
+                        tabuleiro[lin_rand][col_rand] = tabuleiro[lin_rand][col_rand+1] = tabuleiro[lin_rand-1][col_rand+1]= 1;
+                        embarcacoes++;
+                        cout << "Embarcação: " << lin_rand << " " << col_rand << endl;
+                    }
+            }
+        }
+        while(bombas<10)
+        {
+            col_rand = (rand()%40);
+            lin_rand = (rand()%40);
+            if(tabuleiro[lin_rand][col_rand]!=1)
+            {
+                tabuleiro[lin_rand][col_rand] = 2;
+                bombas++;
+                cout << "Bomba: " << lin_rand << " " << col_rand << endl;
+            }
+        } 
+    }
+    else
+    {
+        for(int i=0;i<40; i++)
+        {
+            for(int j=0; j<40; j++)
+            {
+                if(tabuleiro[i][j]==3)
+                {
+                    glColor3f(0.0f, 0.0f, 0.9f); 
+                    DesenhaQuadrado(5+(11*j),5+(11*j),2+(11*(j+1)),2+(11*(j+1)),11*(i+1),2+(11*i),2+(11*i),11*(i+1)); 
+                } 
+                else if(tabuleiro[i][j]==4)
+                {
+                    glColor3f(0.9f, 0.f, 0.0f); 
+                    DesenhaQuadrado(5+(11*j),5+(11*j),2+(11*(j+1)),2+(11*(j+1)),11*(i+1),2+(11*i),2+(11*i),11*(i+1)); 
+                }
+                else
+                {
+                    glColor3f(0.91f, 0.91f, 0.70f); 
+                    DesenhaQuadrado(5+(11*j),5+(11*j),2+(11*(j+1)),2+(11*(j+1)),11*(i+1),2+(11*i),2+(11*i),11*(i+1)); 
+                }
+            }
+        }
+        glFlush();
+    }
+    return;
+}
+
+void BatalhaNaval(int x, int y)
+{
+    ganhou = true;
+    for(int i=0; i<40; i++)
     {
         for(int j=0; j<40; j++)
         {
-            DesenhaQuadrado(5+(11*j),5+(11*j),2+(11*(j+1)),2+(11*(j+1)),11*(i+1),2+(11*i),2+(11*i),11*(i+1)); 
-            tabuleiro[i][j] = 0;
-        }
-    }
-    int col_rand,lin_rand, embarcacao_rand;
-    while(bombas<10)
-    {
-        col_rand = (rand()%40);
-        lin_rand = (rand()%40);
-        tabuleiro[lin_rand][col_rand] = 1;
-        glColor3f(0.91f, 0.0f, 0.0f); 
-        DesenhaQuadrado(5+(11*col_rand),5+(11*col_rand),2+(11*(col_rand+1)),2+(11*(col_rand+1)),11*(lin_rand+1),2+(11*lin_rand),2+(11*lin_rand),11*(lin_rand+1)); 
-        bombas++;
-    }
-    while(embarcacoes<10)
-    {
-        col_rand = (rand()%40);
-        lin_rand = (rand()%40);
-        if(tabuleiro[lin_rand][col_rand] != 1)
-        {
-            embarcacao_rand = rand()%4;
-            /*switch(embarcacao_rand)
+            if(tabuleiro[i][j] == 1)
             {
-                case 0: //Destroyer
-                //se coluna menor que 40-length
-                case 1: //Porta-aviões
-                case 2: //Lança de ataque
-                case 3: //Submarino
-                case 4: //Corveta
-            }*/
+                ganhou = false;
+            } 
         }
     }
-    
+    if(!ganhou)
+    {
+        TelaBatalhaNaval();
+        if(!primeiro_desenho)
+        { 
+            int j = (x-5)/11, i = (y-2)/11;
+            if(tabuleiro[i][j]==1)
+            {
+                tabuleiro[i][j] = 3; 
+            }
+            else if(tabuleiro[i][j]==2)
+            {
+                tabuleiro[i][j] = 4; 
+                vidas--;
+            } 
+            glFlush();
+            glutPostRedisplay();
+        }     
+    }
+    else
+    {
+        sprintf(texto, "Game Over - You win");
+        textDraw();
+        tipo = 0;
+    }
+    if (vidas == 0)
+    {
+        sprintf(texto, "Game Over - You lose");
+        cout << texto << endl;
+        textDraw();
+        //tipo = 0;
+    }
+    return;
 }
 
 void Desenha(void)
@@ -347,6 +468,7 @@ void Desenha(void)
         break;
     case 5:
         TelaBatalhaNaval();
+        break;
     }
     glFlush();
 }
@@ -393,6 +515,8 @@ void MenuPrincipal(int op)
     {
         case 2:
             tipo = 5;
+            vidas = 3;
+            primeiro_desenho = true;
             break;
         case 3:
             glutDestroyWindow(glutGetWindow());
@@ -455,6 +579,8 @@ void HandleMouse(int button, int state, int x, int y)
                 break;
             case 2:
                 MemoriaDificil(xclick,yclick);
+            case 5:
+                BatalhaNaval(xclick, yclick);
                 break;
         }
 	}
